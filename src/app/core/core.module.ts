51467d9a3c352d62a3f707cb5f299cb88dfb944c
dnyuhm <dylanmeuriot@gmail.com>
2022-05-31 14:33:06 +0200

    Commit 17 : Navigation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,20 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { UiModule } from '../ui/ui.module';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [
+  declarations: [HeaderComponent, FooterComponent, NavComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    LoginModule,
+    IconsModule,
+    UiModule,
+    TemplatesModule,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
   ],
-  imports: [CommonModule],
-  exports: [LoginModule, IconsModule, UiModule, TemplatesModule, HeaderComponent, FooterComponent, NavComponent],
 })
 export class CoreModule {}
