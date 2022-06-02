f39565f9f1cc5924f2414f69954dbcc61f907899
dnyuhm <dylanmeuriot@gmail.com>
2022-06-02 14:19:58 +0200

    Commit 29 : StateDirective & hostBinding

---
+++ src/app/shared/directives/state.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, HostBinding, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appState]',
+})
+export class StateDirective {
+  @Input() appState!: string;
+  @HostBinding('class') tdClassName!: string;
+
+  constructor() {}
+
+  ngOnChanges(): void {
+    console.log(this.appState);
+    this.tdClassName = `state-${this.appState.toLowerCase()}`;
+  }
+}
